64c030cc66a1bd1d7ab5fc3a5a4977414e7b4605
ajovicic019 <ajovicic019@gmail.com>
2023-05-29 12:49:26 +0200

    Made search engine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import "./App.css";
 import logo from "./logo.png";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App container">
       <header className="App-header">
         <img src={logo} alt="SheCodesc logo" className="logo img-fluid" />
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer>Coded by Andrea Jovicic</footer>
     </div>
   );
 }
